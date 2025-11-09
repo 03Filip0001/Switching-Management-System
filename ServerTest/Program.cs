9a5999ea38a2b21582000d5b3464cd1f63cd5e59
03Filip0001 <hackboyiv@gmail.com>
2025-11-09 20:00:05 +0100

    Base server for testing connection

---
+++ ServerTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            WorkPlanClass.WorkPlanClass reply = client.GetWorkPlan();
+            Console.WriteLine(reply);
+            client.Close();
+        }
+    }
+}
