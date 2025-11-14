8c0fac52a2e5743fbd0c25a73e127c388a172fb4
03Filip0001 <hackboyiv@gmail.com>
2025-11-14 08:40:01 +0100

    Base instruction class

---
+++ WorkPlanClass/InstructionsClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkPlanClass
+{
+    internal class Switches
+    {
+        public int Switch_ID;
+        public bool state;
+        public Switches() { }
+    }
+
+    internal class Instruction
+    {
+        public int Number;
+        List<Switches> Switches;
+        public Instruction() {
+            Switches = new List<Switches>();
+        }
+    }
+    internal class InstructionsClass
+    {
+        List<Instruction> Instructions;
+        public InstructionsClass() {
+            Instructions = new List<Instruction>();
+        }
+    }
+}
