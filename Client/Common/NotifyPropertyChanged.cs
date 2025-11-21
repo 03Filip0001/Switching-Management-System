2a7cbec08cb85db6c24595629f1e4d82ba84f102
03Filip0001 <hackboyiv@gmail.com>
2025-11-21 09:11:00 +0100

    Added NotifyPropertyChanged class to client

---
+++ Client/Common/NotifyPropertyChanged.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Mini_Switching_Management_System_Client.Common
+{
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null!)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
