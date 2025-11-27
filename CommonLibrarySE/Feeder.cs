0ba8bda06a04546630e84d10732d20fa2446b50d
03Filip0001 <hackboyiv@gmail.com>
2025-11-27 14:53:46 +0100

    Added substation and feeder class

---
+++ CommonLibrarySE/Feeder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace CommonLibrarySE
+{
+    class Feeder
+    {
+        private int _ID;
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        private ObservableCollection<Switch> _Switches;
+        public ObservableCollection<Switch> Switches
+        {
+            get { return _Switches; }
+            set { _Switches = value; }
+        }
+    }
+}
---
+++ CommonLibrarySE/Substation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace CommonLibrarySE
+{
+    class Substation
+    {
+        private int _ID;
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        private ObservableCollection<Feeder> _Feeders;
+        public ObservableCollection<Feeder> Feeders
+        {
+            get { return _Feeders; }
+            set { _Feeders = value; }
+        }
+    }
+}
