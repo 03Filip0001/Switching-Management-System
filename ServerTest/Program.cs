9b143354cc4c8c781ef00dbf2ac14be97ed2befb
03Filip0001 <hackboyiv@gmail.com>
2025-12-03 19:13:35 +0100

    Test substation

--- ServerTest/Program.cs
+++ ServerTest/Program.cs
@@ -14,15 +14,32 @@ namespace ServerTest
 
             ObservableCollection<Server.Substation> substations = client.GetSubstations();
 
+            if (substations.Count == 0)
+            {
+                substations.Add(new Server.Substation());
+            }
+
             substations[0].Feeders = new ObservableCollection<Server.Feeder>();
 
-            Server.Switch s1 = new Server.Switch { Switch_ID = 1};
-            Server.Switch s2 = new Server.Switch { Switch_ID = 2};
-            Server.Feeder f1 = new Server.Feeder { ID = 0, Name = "test", Switches = new ObservableCollection<Server.Switch>()};
+            Server.Switch s1 = new Server.Switch { Switch_ID = 1, State=true};
+            Server.Switch s2 = new Server.Switch { Switch_ID = 2, State = false};
+            Server.Switch s3 = new Server.Switch { Switch_ID = 3, State=false};
+
+            Server.Switch s4 = new Server.Switch { Switch_ID = 4, State = true };
+            Server.Switch s5 = new Server.Switch { Switch_ID = 5, State = true };
+
+            Server.Feeder f1 = new Server.Feeder { ID = 0, Name = "Petrovaradin", Switches = new ObservableCollection<Server.Switch>()};
+            Server.Feeder f2 = new Server.Feeder { ID = 1, Name = "Novi Sad", Switches = new ObservableCollection<Server.Switch>() };
+
             f1.Switches.Add(s1);
             f1.Switches.Add(s2);
+            f1.Switches.Add(s3);
+
+            f2.Switches.Add(s4);
+            f2.Switches.Add(s5);
 
             substations[0].Feeders.Add(f1);
+            substations[0].Feeders.Add(f2);
 
             bool res = client.SaveSubstation(substations[0]);
             client.Close();
